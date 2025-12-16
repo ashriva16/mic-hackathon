b5cec0e65b0bde5dd3602b3a77dd228880324a74
maclariz <ian.maclaren@glasgow.ac.uk>
2025-12-16 10:07:32 +0000

    Created dataloader.py
    Initial creation of dataloader.py which will contain all dataloader functions. No __init__.py yet until we know what functions need to appear in the tree.

---
+++ 4denoiser/dataloader/dataloader.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+'''
+This module contains all utilities needed to load training data and the live diffraction pattern for denoising into pytorch 
+
+Functions will be added in due course
+
+All assumes that data is held in memory as numpy files currently
+
+If necessary, we could investigate modifying this to holding data as cupy arrays if there is a suitable GPU with enough memory to hold these
+'''
